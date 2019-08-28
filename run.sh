62e469594c7a19a2235ba7b6542c3195a6d70ced
Tomi Mickelsson <atomi@iki.fi>
2019-08-28 19:24:27 +0300

    run.sh update for docker

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# run uwsgi in dev mode
+# run in dev mode
 
-uwsgi --ini conf/uwsgi.ini:uwsgi-debug
+# uwsgi --ini conf/uwsgi.ini:uwsgi-debug
+docker run --rm --name restpie-dev -p 8100:80 -v ~/Downloads/restpie3/py:/app/pylocal restpie-dev-image
 
