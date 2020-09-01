eefffcd18fc74215a00bfe6de58364963b4c38bc
Tomi Mickelsson <atomi@iki.fi>
2020-09-01 19:42:08 +0300

    Scripts run and rsync

---
+++ rsync.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# rsync files to server and reload
+
+# HOST: replace with your real data
+HOST='pi@192.168.100.10'
+
+echo "RSYNCING in 3secs..."
+sleep 3
+
+rsync -av --exclude '.git' --exclude '__pycache__' --exclude '*.pyc' * $HOST:/app/
+
+# ask python server to reload sources
+ssh $HOST touch /app/VERSION
+
